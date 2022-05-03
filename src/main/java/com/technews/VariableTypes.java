a65d541658780324eddf84dcdb118caa371ba292
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-05-02 21:22:29 -0400

    added models

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Rebecca";
+        int age = 2078 + 23;
+        double price = 223.45;
+        boolean truth = true;
+        boolean falseness = !truth;
+    }
+}
---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+       int num = 23;
+       double dec = 23.23;
+       double sum = num + dec;
+       System.out.println(sum);
+
+       String name = "Rebecca";
+       String together = name + num;
+       System.out.println(together);
+    }
+}
